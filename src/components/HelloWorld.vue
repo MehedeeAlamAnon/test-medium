1945ed3ecc47421aff640aad52fb5315d2ef311a
MehedeeAlamAnon <36200610+MehedeeAlamAnon@users.noreply.github.com>
2021-03-05 00:20:12 +0600

    anon

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -41,7 +41,8 @@ export default {
   data() {
     return {
       url: process.env.VUE_APP_URL,
-      title: process.env.VUE_APP_TITLE
+      title: process.env.VUE_APP_TITLE,
+      newProducts:[],
     }
   },
   props: {
@@ -56,15 +57,64 @@ export default {
       //   console.table(this.branches)
       // })
 
-      axios.get(this.url+'api/branch').then(response=>{
-        console.log(response.data.data)
-        this.branches=response.data.data
-        console.table(this.branches)
-      })
+
+      // axios.get(this.url+'api/branch').then(response=>{
+      //   console.log(response.data.data)
+      //   this.branches=response.data.data
+      //   console.table(this.branches)
+      // })
+
+      let branchID=localStorage.branchID
+      console.log(branchID)
+
+      // axios.get(this.url+'/api/category/branch/' + branchID).then(response=>{
+      //   console.log(response.data.info)
+      //   //this.branches=response.data.data
+      //   //console.table(this.branches)
+      // })
+
+
+      // axios.get('http://developer.amanabigbazar.com/api/category/branch/' + branchID).then(response=>{
+      //   console.log(response.data.info)
+      //   //this.branches=response.data.data
+      //   //console.table(this.branches)
+      // })
+
+
+
+
+      setTimeout(()=>{
+        console.log("Calling Get Special offer Product")
+        // let requestLink=this.url+'api/product/search/list/1' + '?branch='+ localStorage.branchID
+
+        //let requestLink='/api/product/search/list/1' + '?branch='+ localStorage.branchID
+        let requestLink='http://developer.amanabigbazar.com/api/product/search/list/1' + '?branch='+ localStorage.branchID
+        axios.post(requestLink, {
+          category:null,
+          subcategory:null,
+          brand:null,
+          supplier:null,
+          name:null,
+          min_price:null,
+          max_price:1000000,
+          new_product: false,
+          special_offer: true
+        }).then( response => {
+          if (response.data.data.length) {
+            console.log("Found New Product")
+            this.newProducts=response.data.data
+            console.log(this.newProducts)
+          } else {
+            console.log()
+          }
+        });
+      },2000)
     },
   },
 
   mounted() {
+    localStorage.setItem('branchID','5f82a8635e989e064902d02e')
+    localStorage.setItem('userToken','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjAzNTIwY2VkNzMzMTExNGRkMjU3OTYyIn0sImlhdCI6MTYxNDUxMzUxNywiZXhwIjoxNjE0ODczNTE3fQ.nnaOStkkjRIrCpICISgIVrtNzWfaFYwuqIOz9in8it0')
     this.fetchData()
     console.log(this.url)
   }
